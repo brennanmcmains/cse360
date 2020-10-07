5c6e1960f3e59eeb3b8c653a082265194ead3fba
brennanmcmains <71227133+brennanmcmains@users.noreply.github.com>
2020-10-07 12:50:13 -0700

    Add files via upload
    Added comments explaining the methods

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {		//A method that returns the total
+    return 0;
+  }
+  
+  public void add (int value) { //Method that adds a passed integer to the total variable
+  }
+
+  public void subtract (int value) { //Method that subtracts a passed integer from the total variable
+  }
+
+  public String toString () {	//A method that returns the history of transactions
+    return "";
+  }
+
+  public void clear() {		//A method that clears the variables of AddingMachine
+  }
+}
