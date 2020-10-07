a66279a57352b4904ad62415b8c4c46c42968ab8
brennanmcmains <71227133+brennanmcmains@users.noreply.github.com>
2020-10-07 12:56:08 -0700

    Add files via upload
    Implemented add, subtract, toString, clear, and getTotal.

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {		//A method that returns the total
-    return 0;
+    return total;
   }
   
   public void add (int value) { //Method that adds a passed integer to the total variable
+	  total = total + value;
+	  history += " + " + value;
   }
 
   public void subtract (int value) { //Method that subtracts a passed integer from the total variable
+	  total = total - value;
+	  history += " - " + value;
+
   }
 
   public String toString () {	//A method that returns the history of transactions
-    return "";
+    return history;
   }
 
   public void clear() {		//A method that clears the variables of AddingMachine
+  total = 0;
+  history = "0";
   }
 }
